77ff04dbc44f55c4f4aeacdf5365b75987914a7d
eash-codes <forwork8958@gmail.com>
2025-11-05 23:21:10 +0530

    Initialize D lib Detector and predictor and indices for eye landmarks

--- drowsiness_EAR.py
+++ drowsiness_EAR.py
@@ -14,3 +14,11 @@ def eye_aspect_ratio(eye_points):
     ear = (vertical_1 + vertical_2)/(2.0 * horizontal)
 
     return ear
+
+# initialize dlibs face dtector and landmark predictor 
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+#sooo indeces for eye landmarks from 68 points model 
+LEFT_EYE_INDICES = [36,37,38,39,40,41]
+RIGHT_EYE_INDICES = [42,43,44,45,46,47]
